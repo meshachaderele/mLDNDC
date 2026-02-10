659db24fb94074ba29a95fb6de7c101cc8d7dba7
meshachaderele <meshachaderele@gmail.com>
2026-02-10 13:57:39 +0100

    updated directories

--- data_processing/add_prec_days.py
+++ data_processing/add_prec_days.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_precipitation_days():
 
-    df = pd.read_csv("../data/climate_10km_2010_2020.csv")
+    df = pd.read_csv("data/climate_10km_2010_2020.csv")
     
     df['date'] = pd.to_datetime(df['date'])
     df['year'] = df['date'].dt.year   
--- data_processing/finalize.py
+++ data_processing/finalize.py
@@ -5,8 +5,8 @@ import gc
 def finalize(CROP):
     df = cudf.read_parquet(f'data/ml_ready_{CROP}_outputs_cleaned.parquet') 
     clim = cudf.read_parquet(f'data/simulation_ids_{CROP}_with_climate.parquet')
-    soil = cudf.read_parquet('../data/soil_data_with_classes.parquet')
-    climate_df = cudf.read_csv('../data/climate_10km_2010_2020.csv')
+    soil = cudf.read_parquet('data/soil_data_with_classes.parquet')
+    climate_df = cudf.read_csv('data/climate_10km_2010_2020.csv')
     prec_days = cudf.read_csv('data/climate_prec_days.csv')
     df = df.merge(clim, left_on=['id', 'year'], right_on=['event_id', 'harvest_year'])
     del clim
